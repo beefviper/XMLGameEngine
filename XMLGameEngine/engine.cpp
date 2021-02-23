90734f1fe30d3e8e2f2c0ce4dbab01963d91e00e
Michael Lefebvre <beef.viper@gmail.com>
2021-02-22 23:20:58 -0500

    wrapped all code in xge namespace, removed excess xge:: scope resolution

--- XMLGameEngine/engine.cpp
+++ XMLGameEngine/engine.cpp
@@ -5,162 +5,164 @@
 
 #include "engine.h"
 
-xge::Engine::Engine(xge::Game& game) :
-	game(game)
-{
-	xge::WindowDesc windowDesc = game.getWindowDesc();
-
-	std::string name = windowDesc.name;
-
-	int width = static_cast<int>(windowDesc.width);
-	int height = static_cast<int>(windowDesc.height);
-	sf::VideoMode videoMode(width, height);
-
-	auto windowMode = (windowDesc.fullscreen == "true") ? sf::Style::Fullscreen : sf::Style::Default;
-
-	window.create(videoMode, name, windowMode);
-	window.setFramerateLimit(windowDesc.framerate);
-}
-
-void xge::Engine::loop(void)
-{
-	while (window.isOpen())
+namespace xge {
+	Engine::Engine(Game& game) :
+		game(game)
 	{
-		sf::Event event;
-		while (window.pollEvent(event))
+		WindowDesc windowDesc = game.getWindowDesc();
+
+		std::string name = windowDesc.name;
+
+		int width = static_cast<int>(windowDesc.width);
+		int height = static_cast<int>(windowDesc.height);
+		sf::VideoMode videoMode(width, height);
+
+		auto windowMode = (windowDesc.fullscreen == "true") ? sf::Style::Fullscreen : sf::Style::Default;
+
+		window.create(videoMode, name, windowMode);
+		window.setFramerateLimit(windowDesc.framerate);
+	}
+
+	void Engine::loop(void)
+	{
+		while (window.isOpen())
 		{
-			switch (event.type)
+			sf::Event event;
+			while (window.pollEvent(event))
 			{
-			case sf::Event::Closed:
-				window.close();
-				break;
+				switch (event.type)
+				{
+				case sf::Event::Closed:
+					window.close();
+					break;
 
-			case sf::Event::KeyPressed:
-				handleKeyPressed(game, event);
-				break;
+				case sf::Event::KeyPressed:
+					handleKeyPressed(game, event);
+					break;
 
-			case sf::Event::KeyReleased:
-				handleKeyReleased(game, event);
-				break;
+				case sf::Event::KeyReleased:
+					handleKeyReleased(game, event);
+					break;
 
-			default:
-				break;
+				default:
+					break;
+				}
+			}
+
+			game.updateObjects();
+
+			window.clear(sfmlColor(game.getWindowDesc().background));
+
+			for (auto& name : game.getCurrentState().show)
+			{
+				window.draw(*game.getObject(name).sprite);
+			}
+
+			window.display();
+		}
+	}
+
+	void Engine::handleKeyPressed(Game& game, sf::Event& event)
+	{
+		if (!isKeyPressed[event.key.code])
+		{
+			isKeyPressed[event.key.code] = true;
+
+			for (auto& input : game.getCurrentState().input)
+			{
+				execute_action(game, event, input, true);
 			}
 		}
-
-		game.updateObjects();
-
-		window.clear(xge::sfmlColor(game.getWindowDesc().background));
-
-		for (auto& name : game.getCurrentState().show)
-		{
-			window.draw(*game.getObject(name).sprite);
-		}
-
-		window.display();
 	}
-}
 
-void xge::Engine::handleKeyPressed(xge::Game& game, sf::Event& event)
-{
-	if (!isKeyPressed[event.key.code])
+	void Engine::handleKeyReleased(Game& game, sf::Event& event)
 	{
-		isKeyPressed[event.key.code] = true;
+
+		isKeyPressed[event.key.code] = false;
 
 		for (auto& input : game.getCurrentState().input)
 		{
-			execute_action(game, event, input, true);
+			execute_action(game, event, input, false);
 		}
 	}
-}
 
-void xge::Engine::handleKeyReleased(xge::Game& game, sf::Event& event)
-{
-
-	isKeyPressed[event.key.code] = false;
-
-	for (auto& input : game.getCurrentState().input)
+	void Engine::execute_action(Game& game, sf::Event& event, PairStringString& input, bool keyPressed)
 	{
-		execute_action(game, event, input, false);
-	}
-}
-
-void xge::Engine::execute_action(xge::Game& game, sf::Event& event, PairStringString& input, bool keyPressed)
-{
-	if (input.second == sfmlKeyToString(event.key.code))
-	{
-		std::string aObject = getObjectFromInput(input);
-		std::string aCommand = getCommandFromInput(input);
-
-		if (aObject == "state")
+		if (input.second == sfmlKeyToString(event.key.code))
 		{
-			if (keyPressed)
+			std::string aObject = getObjectFromInput(input);
+			std::string aCommand = getCommandFromInput(input);
+
+			if (aObject == "state")
 			{
-				game.setCurrentState(aCommand);
+				if (keyPressed)
+				{
+					game.setCurrentState(aCommand);
+				}
+			}
+			else if (&game.getObject(aObject) != nullptr)
+			{
+				std::string aAction = game.getObject(aObject).action[aCommand];
+				auto aStep = getValueFromAction(aAction);
+
+				if (!keyPressed)
+				{
+					aStep = 0;
+				}
+
+				move(game.getObject(aObject), aCommand, aStep);
 			}
 		}
-		else if (&game.getObject(aObject) != nullptr)
+	}
+
+	void Engine::move(Object& object, Engine::Direction direction, float step)
+	{
+		switch (direction)
 		{
-			std::string aAction = game.getObject(aObject).action[aCommand];
-			auto aStep = getValueFromAction(aAction);
-
-			if (!keyPressed)
-			{
-				aStep = 0;
-			}
-
-			move(game.getObject(aObject), aCommand, aStep);
+		case Direction::Up:
+			object.velocity.y = -step;
+			break;
+		case Direction::Down:
+			object.velocity.y = step;
+			break;
+		case Direction::Left:
+			object.velocity.x = -step;
+			break;
+		case Direction::Right:
+			object.velocity.x = step;
+			break;
 		}
 	}
-}
 
-void xge::Engine::move(xge::Object& object, Engine::Direction direction, float step)
-{
-	switch (direction)
+	void Engine::move(Object& object, std::string direction, float step)
 	{
-	case Direction::Up:
-		object.velocity.y = -step;
-		break;
-	case Direction::Down:
-		object.velocity.y = step;
-		break;
-	case Direction::Left:
-		object.velocity.x = -step;
-		break;
-	case Direction::Right:
-		object.velocity.x = step;
-		break;
+		move(object, mapDirection[direction], step);
 	}
-}
 
-void xge::Engine::move(xge::Object& object, std::string direction, float step)
-{
-	move(object, mapDirection[direction], step);
-}
+	std::string Engine::getObjectFromInput(PairStringString input)
+	{
+		auto sStart = 0;
+		auto sEnd = input.first.find(".");
 
-std::string xge::Engine::getObjectFromInput(PairStringString input)
-{
-	auto sStart = 0;
-	auto sEnd = input.first.find(".");
+		return input.first.substr(sStart, sEnd);
+	}
 
-	return input.first.substr(sStart, sEnd);
-}
+	std::string Engine::getCommandFromInput(PairStringString input)
+	{
+		auto sStart = input.first.rfind('.') + 1;
+		auto sEnd = input.first.length();
+		auto sLength = sEnd - sStart;
 
-std::string xge::Engine::getCommandFromInput(PairStringString input)
-{
-	auto sStart = input.first.rfind('.') + 1;
-	auto sEnd = input.first.length();
-	auto sLength = sEnd - sStart;
+		return input.first.substr(sStart, sLength);
+	}
 
-	return input.first.substr(sStart, sLength);
-}
+	float Engine::getValueFromAction(std::string action)
+	{
+		auto sStart = action.find("(") + 1;
+		auto sEnd = action.find(")");
+		auto sLength = sEnd - sStart;
+		auto sVar = action.substr(sStart, sLength);
 
-float xge::Engine::getValueFromAction(std::string action)
-{
-	auto sStart = action.find("(") + 1;
-	auto sEnd = action.find(")");
-	auto sLength = sEnd - sStart;
-	auto sVar = action.substr(sStart, sLength);
-
-	return game.getVariable(sVar);
+		return game.getVariable(sVar);
+	}
 }
