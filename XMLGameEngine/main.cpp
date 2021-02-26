3b4b275daca27cbe26e4823b5f74e9d79151eed7
Michael Lefebvre <beef.viper@gmail.com>
2021-02-26 02:12:38 -0500

    Fixed up library includes in cmake file

--- XMLGameEngine/main.cpp
+++ XMLGameEngine/main.cpp
@@ -9,16 +9,103 @@
 #include <iostream>
 #include <string>
 
-int main(void)
+#include <stdio.h>
+
+#include <xercesc/util/XMLString.hpp>
+#include <xercesc/parsers/XercesDOMParser.hpp>
+#include <xercesc/framework/LocalFileInputSource.hpp>
+#include <xercesc/sax/ErrorHandler.hpp>
+#include <xercesc/sax/SAXParseException.hpp>
+#include <xercesc/validators/common/Grammar.hpp>
+
+using namespace xercesc;
+
+class ParserErrorHandler : public ErrorHandler
 {
-	std::string filename{ "games/pong.xml" };
-	xge::Game game{ filename };
+private:
+    void reportParseException(const SAXParseException& ex)
+    {
+        char* msg = XMLString::transcode(ex.getMessage());
+        fprintf(stderr, "at line %llu column %llu, %s\n",
+            ex.getColumnNumber(), ex.getLineNumber(), msg);
+        XMLString::release(&msg);
+    }
 
-	game.printGame();
-	game.setCurrentState(0);
+public:
+    void warning(const SAXParseException& ex)
+    {
+        reportParseException(ex);
+    }
 
-	xge::Engine engine(game);
-	engine.loop();
+    void error(const SAXParseException& ex)
+    {
+        reportParseException(ex);
+    }
 
-	return EXIT_SUCCESS;
+    void fatalError(const SAXParseException& ex)
+    {
+        reportParseException(ex);
+    }
+
+    void resetErrors()
+    {
+    }
+};
+
+void ValidateSchema(const char* schemaFilePath, const char* xmlFilePath)
+{
+    XercesDOMParser domParser;
+    if (domParser.loadGrammar(schemaFilePath, Grammar::SchemaGrammarType) == NULL)
+    {
+        fprintf(stderr, "couldn't load schema\n");
+        return;
+    }
+
+    ParserErrorHandler parserErrorHandler;
+
+    domParser.setErrorHandler(&parserErrorHandler);
+    domParser.setValidationScheme(XercesDOMParser::Val_Auto);
+    domParser.setDoNamespaces(true);
+    domParser.setDoSchema(true);
+    domParser.setValidationConstraintFatal(true);
+
+    domParser.parse(xmlFilePath);
+    if (domParser.getErrorCount() == 0)
+        printf("XML file validated against the schema successfully\n");
+    else
+        printf("XML file doesn't conform to the schema\n");
 }
+
+int main(int argc, const char* argv[])
+{
+    if (argc != 3)
+    {
+        printf("SchemaValidator <schema file> <xml file>\n");
+        return 0;
+    }
+
+    XMLPlatformUtils::Initialize();
+
+    ValidateSchema(argv[1], argv[2]);
+
+    XMLPlatformUtils::Terminate();
+
+    return 0;
+}
+
+
+
+
+//int main(void)
+//{
+//	std::string filename{ "games/pong.xml" };
+//	xge::Game game{ filename };
+//
+//	game.printGame();
+//	game.setCurrentState(0);
+//
+//	xge::Engine engine(game);
+//	engine.loop();
+//
+//	return EXIT_SUCCESS;
+//}
